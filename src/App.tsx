044b1e6a59fca8747399982c52c640013233c504
Austin Theriot <austinmtheriot@gmail.com>
2020-09-16 12:31:56 -0500

    added material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import styles from './App.module.scss';
 
 import logo from './images/chirho-light.svg';
 
 function App() {
 	return (
 		<div className='App'>
-			<img src={logo} alt='logo' />
+			<img src={logo} alt='logo' className={styles.logo} />
+			<h1>Scripture Memorization</h1>
+			<p>
+				P,aaoCJbtwoG, TtswaiE,aafiCJ: GtyapfGoFatLJC.
+				BbtGaFooLJC,whbuiCwesbithp. Fhcuihbtfotw,twsbhabbh.
+				ilhpufathastJC,attpohw, ttpohgg,wwhhbuitB. ihwhrthb,tfoot,attrohg,whluu.
+				iawai,hmktutmohw,athp, whsfiCaapftfot, tuatih,tihatoe.
+				ihwhoai,hbpattpohwwatattcohw, stwwwtfthiCmbttpohg.
+				ihya,wyhtwot,tgoys,abih, wswtpHS--witgooiuwapoi--ttpohg.
+				Ftr,bihhoyfitLJayltats, idnctgtfy,ryimp, ttGooLJC,tFog,mgytSowaoritkoh.
+				iptteoyhmbe,tymkwithtwhhcy, watrohgiits,awitigohptuwb. TaiawtwohgmthwiC
+				whrhftdashahrhithp, faaraaapad,aaentin, noitabaitotc.
+				Ahpatuhfaghahoatttc, wihb,tfohwfaia.
+			</p>
+			<h2>Copyright Notice:</h2>
+			<p>
+				Scripture quotations are from the ESV® Bible (The Holy Bible, English
+				Standard Version®), copyright © 2001 by Crossway, a publishing ministry
+				of Good News Publishers. Used by permission. All rights reserved. You
+				may not copy or download more than 500 consecutive verses of the ESV
+				Bible or more than one half of any book of the ESV Bible.
+			</p>
 		</div>
 	);
 }
