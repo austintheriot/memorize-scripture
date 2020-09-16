6d474c19b124e540a48e1ec03fce1b73ab92426b
Austin Theriot <austinmtheriot@gmail.com>
2020-09-16 11:59:53 -0500

    added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import logo from './images/chirho-light.svg';
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<img src={logo} alt='logo' />
+		</div>
+	);
 }
 
 export default App;
