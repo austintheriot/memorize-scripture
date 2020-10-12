de1ace4f73022d75451b248c69837d8a84b99043
Austin Theriot <austinmtheriot@gmail.com>
2020-10-12 14:32:37 -0500

    added styling

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -7,9 +7,11 @@ import flipIcon from '../icons/flip.svg';
 export const Footer = (props: any) => {
 	return (
 		<footer className={styles.Footer}>
-			<button className={styles.buttons} onClick={props.flipView}>
-				<img src={flipIcon} alt={'change view'}></img>
-			</button>
+			<div className={styles.ButtonContainer}>
+				<button className={styles.buttons} onClick={props.flipView}>
+					<img src={flipIcon} alt={'change view'}></img>
+				</button>
+			</div>
 		</footer>
 	);
 };
