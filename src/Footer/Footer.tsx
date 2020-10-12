fac12ca583c86a82ac2d9ccc45c80b5be889dcef
Austin Theriot <austinmtheriot@gmail.com>
2020-10-12 13:51:52 -0500

    added footer styling

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import styles from './Footer.module.scss';
 
 export const Footer = () => {
-	return <footer></footer>;
+	return (
+		<footer className={styles.Footer}>
+			<button className={styles.buttons}>Flip</button>
+		</footer>
+	);
 };
