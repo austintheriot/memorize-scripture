c81f1c781ef2cd03a64e6a55dbca49a31b551e6e
Austin Theriot <austinmtheriot@gmail.com>
2020-10-13 15:48:59 -0500

    added icons to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,13 +3,29 @@ import styles from './Footer.module.scss';
 
 //Custom icons
 import flipIcon from '../../icons/flip.svg';
+import beginningIcon from '../../icons/beginning.svg';
+import rewindIcon from '../../icons/rewind.svg';
+import pauseIcon from '../../icons/pause.svg';
+import playIcon from '../../icons/play.svg';
 
 export const Footer = (props: any) => {
 	return (
 		<footer className={styles.Footer}>
 			<div className={styles.ButtonContainer}>
 				<button className={styles.buttons} onClick={props.flipView}>
-					<img src={flipIcon} alt={'change view'}></img>
+					<img src={flipIcon} alt={'change view'} />
+				</button>
+				<button className={styles.buttons}>
+					<img src={beginningIcon} alt={'go to beginning'} />
+				</button>
+				<button className={styles.buttons}>
+					<img src={rewindIcon} alt={'rewind'} />
+				</button>
+				<button className={styles.buttons}>
+					<img src={playIcon} alt={'play'} />
+				</button>
+				<button className={styles.buttons}>
+					<img src={pauseIcon} alt={'pause'} />
 				</button>
 			</div>
 		</footer>
