29e813ae8005d173f380d475a828256ff0fe8418
Austin Theriot <austinmtheriot@gmail.com>
2020-10-19 14:37:30 -0500

    created basic breakLines function

--- src/utilities/condenseText.ts
+++ src/utilities/condenseText.ts
@@ -1,106 +1,205 @@
 //ADD line breaks to existing string
 //Original string should be able to be broken up
 
-export default (string: string) => {
-	const MAX_LINE_LENGTH = 75;
-	let newLines: string[] = [];
+//FIRST split text up into good line breaks
 
-	let lines = string.split('\n');
-	for (let i = 0; i < lines.length; i++) {
-		//iterate through text broken by /n
-		let characterCount = 0;
-		let words = lines[i].split(' ');
-		for (let j = 0; j < words.length; j++) {
-			//iterate through every word in each line (replace word with new, single-letter only version)
-			let word = words[j];
-			let newWord = '';
-			let validCharacterNotYetFound = true;
-			for (let k = 0; k < word.length; k++) {
-				//iterate through every letter in the word
-				const ch = word[k];
-				characterCount++;
+//MIN LINE_LENGTH
+//TARGET LINE_LENGTH
+//maxLineLength
 
-				//matches non-letter characters and numbers
-				if (ch.match(/[^A-Za-z0-9_]/)) {
-					//skip over intra-word apostrophes, hyphens, and commas (important for longer numbers)
-					if (
-						(ch === '’' || ch === '-' || ch === ',') &&
-						word[k + 1] && //make sure word[k + 1] is defined before testing to see if it's a letter
-						word[k + 1].match(/\w/) //matches for english letters
-					) {
-						continue;
-					}
+//Split original text into an array, separated by \n
+//Scan until first punctuation -- how many characters?
+//<= MIN_LINE_LENGTH? keep scanning for punctuation
+//>= maxLineLength?
+//If NO punctuation inside the line, split line directly in the middle
+//If punctuation inside the line, split along punctuation
 
-					//adds symbol to word
-					newWord += ch;
+//currentCharacterCount number of characters in between punctuation
+//Add chunks of numbers in to fill up each line:
+//
 
-					if (characterCount <= MAX_LINE_LENGTH && ch === '—' && word[k + 1]) {
-						//special case for word—word (add in the first letter of the next word--SHORT LINES)
-						newWord += word[k + 1];
-					}
+//THEN condense the text
 
-					//ensuring sensible line breaks:
-					//SHORTER LINES ONLY BREAK FOR . ! ? ” ’
-					if (
-						characterCount < MAX_LINE_LENGTH && //short line
-						(ch === '.' || ch === '!' || ch === '?' || ch === '”') &&
-						word[k + 1] !== '’' && //the following character is not a ’
-						word[k + 1] !== '”' && //the following characters are not ”
-						word[k + 2] !== '”' &&
-						word[k + 3] !== '”'
-					) {
-						if (words[j + 1]) newWord += '\n'; //only add line break not the last word in the line
-						characterCount = 0;
-					}
+const insertBreak = (string: string, i: number) => {
+	return string.slice(0, i + 1) + '\n' + string.slice(i + 1);
+};
 
-					//LONGER LINES BREAK FOR MORE MINOR PUNCTUATION
-					else if (
-						characterCount > MAX_LINE_LENGTH && //long line
-						(ch === '.' ||
-							ch === ',' ||
-							ch === '”' ||
-							ch === '’' ||
-							ch === '!' ||
-							ch === '—' ||
-							ch === ')' ||
-							ch === '?') && //break on . , or ”
-						word[k + 1] !== '’' && //the following character is not a ’
-						word[k + 1] !== '”' && //the following characters are not ”
-						word[k + 1] !== ')' && //the following character is not )
-						word[k + 1] !== ',' && //the following character is not ,
-						word[k + 2] !== '”' &&
-						word[k + 3] !== '”'
-					) {
-						if (words[j + 1]) newWord += '\n'; //only add line break not the last word in the line
-						characterCount = 0;
-						if (ch === '—' && word[k + 1]) {
-							//special case for word—word (add in the first letter of the next word--LONG LINES)
-							newWord += word[k + 1];
-						}
-					}
-					continue;
-				}
-
-				//if not a symbol and the character is not skipped over already:
-				//only add the first letter found in the word
-				if (validCharacterNotYetFound) {
-					if (ch === 'I') {
-						newWord += 'i'; //replace capital I with i
-					} else {
-						newWord += ch;
-					}
-					validCharacterNotYetFound = false;
-				}
-			}
-			words[j] = newWord;
+const findItemIndexes = (
+	s: string,
+	regex: any,
+	i?: number,
+	currentCharacterCount?: number
+) => {
+	if (i === undefined) i = 0; //analyze from beginning of string by default
+	if (currentCharacterCount === undefined) currentCharacterCount = s.length; //analyze rest of the string by default
+	const string = s.substr(i, i + currentCharacterCount);
+	const itemIndexes: number[] = [];
+	for (let j = 0; j < string.length; j++) {
+		const ch = string[j];
+		//search first for punctuation only
+		if (ch.match(regex)) {
+			itemIndexes.push(i + j);
 		}
-		//this yields an array where each element is separate by line breaks,
-		//but the element itself also contains the newly added line breaks
-		lines[i] = words.join('');
-		let newLineArray = lines[i].split('\n');
-		newLineArray.forEach((line) => newLines.push(line));
-		//creates a new array in which the array is "flattened"
-		//so that each array element is separated by line breaks
 	}
-	return newLines;
+	return itemIndexes;
+};
+
+const findPunctuationIndexes = (
+	s: string,
+	i?: number,
+	currentCharacterCount?: number
+) => {
+	const regex = new RegExp(/[^A-Za-z0-9 _]/);
+	return findItemIndexes(s, regex, i, currentCharacterCount);
+};
+
+const findSpaceIndexes = (
+	s: string,
+	i?: number,
+	currentCharacterCount?: number
+) => {
+	const regex = new RegExp(' ');
+	return findItemIndexes(s, regex, i, currentCharacterCount);
+};
+
+const findClosest = (
+	string: string,
+	callback: Function,
+	i?: number,
+	currentCharacterCount?: number
+) => {
+	if (i === undefined) i = 0; //analyze from beginning of string by default
+	if (currentCharacterCount === undefined)
+		currentCharacterCount = string.length; //analyze rest of the string by default
+	const center = i + Math.floor(currentCharacterCount / 2);
+	const indexesInString: number[] = callback(string, i, currentCharacterCount);
+
+	const distancesFromCenter = indexesInString.map(
+		(punctuation) => punctuation - center
+	);
+
+	let smallestDistanceFromCenter = Infinity;
+	let arrayIndexOfClosestLocation = -1;
+	distancesFromCenter.forEach((distance, i) => {
+		if (Math.abs(distance) < smallestDistanceFromCenter) {
+			arrayIndexOfClosestLocation = i;
+			smallestDistanceFromCenter = Math.abs(distance);
+		}
+	});
+	let stringIndexOfClosestItem = indexesInString[arrayIndexOfClosestLocation];
+	let characterAtIndex = string[stringIndexOfClosestItem];
+
+	return {
+		smallestDistanceFromCenter,
+		indexesInString,
+		distancesFromCenter,
+		stringIndexOfClosestItem,
+		characterAtIndex,
+	};
+};
+
+const findClosestPunctuation = (
+	string: string,
+	i?: number,
+	currentCharacterCount?: number
+) => {
+	if (i === undefined) i = 0; //analyze from beginning of string by default
+	if (currentCharacterCount === undefined)
+		currentCharacterCount = string.length; //analyze rest of the string by default
+	return findClosest(string, findPunctuationIndexes, i, currentCharacterCount);
+};
+
+const findClosestSpaces = (
+	string: string,
+	i?: number,
+	currentCharacterCount?: number
+) => {
+	if (i === undefined) i = 0; //analyze from beginning of string by default
+	if (currentCharacterCount === undefined)
+		currentCharacterCount = string.length; //analyze rest of the string by default
+	return findClosest(string, findSpaceIndexes, i, currentCharacterCount);
+};
+
+const findBestBreakPoint = (
+	string: string,
+	i?: number,
+	currentCharacterCount?: number
+) => {
+	if (i === undefined) i = 0; //analyze from beginning of string by default
+	if (currentCharacterCount === undefined)
+		currentCharacterCount = string.length; //analyze rest of the string by default
+
+	let punctuation = findClosestPunctuation(string, i, currentCharacterCount);
+	let spaces = findClosestSpaces(string, i, currentCharacterCount);
+
+	//if there is no punctuation with 10 characters of the middle, break the line on a space
+	return punctuation.smallestDistanceFromCenter < 10
+		? punctuation.stringIndexOfClosestItem
+		: spaces.stringIndexOfClosestItem;
+};
+
+export const breakLines = (
+	s: string,
+	minLineLength?: number,
+	maxLineLength?: number
+) => {
+	let string = s;
+	if (minLineLength === undefined) minLineLength = 85;
+	if (maxLineLength === undefined) maxLineLength = 115;
+
+	let currentCharacterCount = 0;
+
+	for (let i = string.length - 1; i >= 0; i--) {
+		const ch = string[i];
+		currentCharacterCount++;
+
+		//TRUE for punctuation (not spaces)
+		if (ch.match(/[^A-Za-z0-9 _]/)) {
+			if (currentCharacterCount > minLineLength) {
+				//long enough, not too long: break on the punctuation
+				if (currentCharacterCount < maxLineLength) {
+					string = insertBreak(string, i);
+					currentCharacterCount = 0;
+				}
+
+				//too long: break on mid-point of the line, either on a space or on punctuation
+				else {
+					//Break in the middle
+					string = insertBreak(
+						string,
+						findBestBreakPoint(string, i, currentCharacterCount)
+					);
+					//Break at the long point
+					string = insertBreak(string, i);
+					currentCharacterCount = 0;
+				}
+
+				//if not long enough
+			}
+		}
+	}
+	console.log(string);
+	return string;
+};
+
+const Ephesians1 =
+	'Blessed be the God and Father of our Lord Jesus Christ, ' +
+	'who has blessed us in Christ with every spiritual blessing in the heavenly places, ' +
+	'even as he chose us in him before the foundation of the world, ' +
+	'that we should be holy and blameless before him. ' +
+	'In love he predestined us for adoption to himself as sons through Jesus Christ, ' +
+	'according to the purpose of his will, to the praise of his glorious grace, ' +
+	'with which he has blessed us in the Beloved. ' +
+	'In him we have redemption through his blood, the forgiveness of our trespasses, ' +
+	'according to the riches of his grace, which he lavished upon us, ' +
+	'in all wisdom and insight making known to us the mystery of his will, ' +
+	'according to his purpose, which he set forth in Christ as a plan for the fullness of time, ' +
+	'to unite all things in him, things in heaven and things on earth.\n\n';
+
+breakLines(Ephesians1, 100, 150);
+breakLines('This is a dumb example');
+
+export const condenseText = (string: string) => {
+	const array: string[] = [''];
+	return array;
 };
