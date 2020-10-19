8b159a136254bdae330c2fee66dd563bafe703be
Austin Theriot <austinmtheriot@gmail.com>
2020-10-19 16:27:47 -0500

    cleaned up defaults, added test chapter imports accounts for combined punctuation

---
+++ src/utilities/testChapters.ts
@@ -0,0 +1,61 @@
+export const Habakkuk1 = `The oracle that Habakkuk the prophet saw.
+
+O LORD, how long shall I cry for help,
+     and you will not hear?
+Or cry to you “Violence!”
+     and you will not save?
+Why do you make me see iniquity,
+     and why do you idly look at wrong?
+Destruction and violence are before me;
+     strife and contention arise.
+So the law is paralyzed,
+     and justice never goes forth.
+For the wicked surround the righteous;
+     so justice goes forth perverted.
+
+
+     “Look among the nations, and see;
+     wonder and be astounded.
+For I am doing a work in your days
+     that you would not believe if told.
+For behold, I am raising up the Chaldeans,
+     that bitter and hasty nation,
+who march through the breadth of the earth,
+     to seize dwellings not their own.
+They are dreaded and fearsome;
+     their justice and dignity go forth from themselves.
+Their horses are swifter than leopards,
+     more fierce than the evening wolves;
+     their horsemen press proudly on.
+Their horsemen come from afar;
+     they fly like an eagle swift to devour.
+They all come for violence,
+     all their faces forward.
+     They gather captives like sand.
+At kings they scoff,
+     and at rulers they laugh.
+They laugh at every fortress,
+     for they pile up earth and take it.
+Then they sweep by like the wind and go on,
+     guilty men, whose own might is their god!”`;
+
+export const Ephesians3 = `For this reason I, Paul, a prisoner of Christ Jesus on behalf of you Gentiles—assuming that you have heard of the stewardship of God’s grace that was given to me for you, how the mystery was made known to me by revelation, as I have written briefly. When you read this, you can perceive my insight into the mystery of Christ, which was not made known to the sons of men in other generations as it has now been revealed to his holy apostles and prophets by the Spirit. This mystery is that the Gentiles are fellow heirs, members of the same body, and partakers of the promise in Christ Jesus through the gospel.
+
+Of this gospel I was made a minister according to the gift of God’s grace, which was given me by the working of his power. To me, though I am the very least of all the saints, this grace was given, to preach to the Gentiles the unsearchable riches of Christ, and to bring to light for everyone what is the plan of the mystery hidden for ages in God, who created all things, so that through the church the manifold wisdom of God might now be made known to the rulers and authorities in the heavenly places. This was according to the eternal purpose that he has realized in Christ Jesus our Lord, in whom we have boldness and access with confidence through our faith in him. So I ask you not to lose heart over what I am suffering for you, which is your glory.
+
+For this reason I bow my knees before the Father, from whom every family in heaven and on earth is named, that according to the riches of his glory he may grant you to be strengthened with power through his Spirit in your inner being, so that Christ may dwell in your hearts through faith—that you, being rooted and grounded in love, may have strength to comprehend with all the saints what is the breadth and length and height and depth, and to know the love of Christ that surpasses knowledge, that you may be filled with all the fullness of God.
+
+Now to him who is able to do far more abundantly than all that we ask or think, according to the power at work within us, to him be glory in the church and in Christ Jesus throughout all generations, forever and ever. Amen.`;
+
+export const Matthew8 = `When he came down from the mountain, great crowds followed him. And behold, a leper came to him and knelt before him, saying, “Lord, if you will, you can make me clean.” And Jesus stretched out his hand and touched him, saying, “I will; be clean.” And immediately his leprosy was cleansed. And Jesus said to him, “See that you say nothing to anyone, but go, show yourself to the priest and offer the gift that Moses commanded, for a proof to them.”
+
+When he had entered Capernaum, a centurion came forward to him, appealing to him, “Lord, my servant is lying paralyzed at home, suffering terribly.” And he said to him, “I will come and heal him.” But the centurion replied, “Lord, I am not worthy to have you come under my roof, but only say the word, and my servant will be healed. For I too am a man under authority, with soldiers under me. And I say to one, ‘Go,’ and he goes, and to another, ‘Come,’ and he comes, and to my servant, ‘Do this,’ and he does it.” When Jesus heard this, he marveled and said to those who followed him, “Truly, I tell you, with no one in Israel have I found such faith. I tell you, many will come from east and west and recline at table with Abraham, Isaac, and Jacob in the kingdom of heaven, while the sons of the kingdom will be thrown into the outer darkness. In that place there will be weeping and gnashing of teeth.” And to the centurion Jesus said, “Go; let it be done for you as you have believed.” And the servant was healed at that very moment.
+
+And when Jesus entered Peter’s house, he saw his mother-in-law lying sick with a fever. He touched her hand, and the fever left her, and she rose and began to serve him. That evening they brought to him many who were oppressed by demons, and he cast out the spirits with a word and healed all who were sick. This was to fulfill what was spoken by the prophet Isaiah: “He took our illnesses and bore our diseases.”
+
+Now when Jesus saw a crowd around him, he gave orders to go over to the other side. And a scribe came up and said to him, “Teacher, I will follow you wherever you go.” And Jesus said to him, “Foxes have holes, and birds of the air have nests, but the Son of Man has nowhere to lay his head.” Another of the disciples said to him, “Lord, let me first go and bury my father.” And Jesus said to him, “Follow me, and leave the dead to bury their own dead.”
+
+And when he got into the boat, his disciples followed him. And behold, there arose a great storm on the sea, so that the boat was being swamped by the waves; but he was asleep. And they went and woke him, saying, “Save us, Lord; we are perishing.” And he said to them, “Why are you afraid, O you of little faith?” Then he rose and rebuked the winds and the sea, and there was a great calm. And the men marveled, saying, “What sort of man is this, that even winds and sea obey him?”
+
+And when he came to the other side, to the country of the Gadarenes, two demon-possessed men met him, coming out of the tombs, so fierce that no one could pass that way. And behold, they cried out, “What have you to do with us, O Son of God? Have you come here to torment us before the time?” Now a herd of many pigs was feeding at some distance from them. And the demons begged him, saying, “If you cast us out, send us away into the herd of pigs.” And he said to them, “Go.” So they came out and went into the pigs, and behold, the whole herd rushed down the steep bank into the sea and drowned in the waters. The herdsmen fled, and going into the city they told everything, especially what had happened to the demon-possessed men. And behold, all the city came out to meet Jesus, and when they saw him, they begged him to leave their region.
+`;
