e64bff8dcf256fb30df12a6e0d264f98f41a49eb
Austin Theriot <austinmtheriot@gmail.com>
2020-10-19 17:00:05 -0500

    added chapters, rearranged them

--- src/utilities/testChapters.ts
+++ src/utilities/testChapters.ts
@@ -1,3 +1,53 @@
+export const Genesis3 = ` Now the serpent was more crafty than any other beast of the field that the LORD God had made.
+
+He said to the woman, “Did God actually say, ‘You shall not eat of any tree in the garden’?” And the woman said to the serpent, “We may eat of the fruit of the trees in the garden, but God said, ‘You shall not eat of the fruit of the tree that is in the midst of the garden, neither shall you touch it, lest you die.’” But the serpent said to the woman, “You will not surely die. For God knows that when you eat of it your eyes will be opened, and you will be like God, knowing good and evil.” So when the woman saw that the tree was good for food, and that it was a delight to the eyes, and that the tree was to be desired to make one wise, she took of its fruit and ate, and she also gave some to her husband who was with her, and he ate. Then the eyes of both were opened, and they knew that they were naked. And they sewed fig leaves together and made themselves loincloths.
+
+And they heard the sound of the LORD God walking in the garden in the cool of the day, and the man and his wife hid themselves from the presence of the LORD God among the trees of the garden. But the LORD God called to the man and said to him, “Where are you?” And he said, “I heard the sound of you in the garden, and I was afraid, because I was naked, and I hid myself.” He said, “Who told you that you were naked? Have you eaten of the tree of which I commanded you not to eat?” The man said, “The woman whom you gave to be with me, she gave me fruit of the tree, and I ate.” Then the LORD God said to the woman, “What is this that you have done?” The woman said, “The serpent deceived me, and I ate.”
+
+The LORD God said to the serpent,
+
+“Because you have done this,
+     cursed are you above all livestock
+     and above all beasts of the field;
+on your belly you shall go,
+     and dust you shall eat
+     all the days of your life.
+I will put enmity between you and the woman,
+     and between your offspring and her offspring;
+he shall bruise your head,
+     and you shall bruise his heel.”
+
+
+     To the woman he said,
+
+“I will surely multiply your pain in childbearing;
+     in pain you shall bring forth children.
+Your desire shall be contrary to your husband,
+     but he shall rule over you.”
+
+
+     And to Adam he said,
+
+“Because you have listened to the voice of your wife
+     and have eaten of the tree
+of which I commanded you,
+     ‘You shall not eat of it,’
+cursed is the ground because of you;
+     in pain you shall eat of it all the days of your life;
+thorns and thistles it shall bring forth for you;
+     and you shall eat the plants of the field.
+By the sweat of your face
+     you shall eat bread,
+till you return to the ground,
+     for out of it you were taken;
+for you are dust,
+     and to dust you shall return.”
+
+
+     The man called his wife’s name Eve, because she was the mother of all living. And the LORD God made for Adam and for his wife garments of skins and clothed them.
+
+Then the LORD God said, “Behold, the man has become like one of us in knowing good and evil. Now, lest he reach out his hand and take also of the tree of life and eat, and live forever—” therefore the LORD God sent him out from the garden of Eden to work the ground from which he was taken. He drove out the man, and at the east of the garden of Eden he placed the cherubim and a flaming sword that turned every way to guard the way to the tree of life.`;
+
 export const Habakkuk1 = `The oracle that Habakkuk the prophet saw.
 
 O LORD, how long shall I cry for help,
@@ -39,14 +89,6 @@ They laugh at every fortress,
 Then they sweep by like the wind and go on,
      guilty men, whose own might is their god!”`;
 
-export const Ephesians3 = `For this reason I, Paul, a prisoner of Christ Jesus on behalf of you Gentiles—assuming that you have heard of the stewardship of God’s grace that was given to me for you, how the mystery was made known to me by revelation, as I have written briefly. When you read this, you can perceive my insight into the mystery of Christ, which was not made known to the sons of men in other generations as it has now been revealed to his holy apostles and prophets by the Spirit. This mystery is that the Gentiles are fellow heirs, members of the same body, and partakers of the promise in Christ Jesus through the gospel.
-
-Of this gospel I was made a minister according to the gift of God’s grace, which was given me by the working of his power. To me, though I am the very least of all the saints, this grace was given, to preach to the Gentiles the unsearchable riches of Christ, and to bring to light for everyone what is the plan of the mystery hidden for ages in God, who created all things, so that through the church the manifold wisdom of God might now be made known to the rulers and authorities in the heavenly places. This was according to the eternal purpose that he has realized in Christ Jesus our Lord, in whom we have boldness and access with confidence through our faith in him. So I ask you not to lose heart over what I am suffering for you, which is your glory.
-
-For this reason I bow my knees before the Father, from whom every family in heaven and on earth is named, that according to the riches of his glory he may grant you to be strengthened with power through his Spirit in your inner being, so that Christ may dwell in your hearts through faith—that you, being rooted and grounded in love, may have strength to comprehend with all the saints what is the breadth and length and height and depth, and to know the love of Christ that surpasses knowledge, that you may be filled with all the fullness of God.
-
-Now to him who is able to do far more abundantly than all that we ask or think, according to the power at work within us, to him be glory in the church and in Christ Jesus throughout all generations, forever and ever. Amen.`;
-
 export const Matthew8 = `When he came down from the mountain, great crowds followed him. And behold, a leper came to him and knelt before him, saying, “Lord, if you will, you can make me clean.” And Jesus stretched out his hand and touched him, saying, “I will; be clean.” And immediately his leprosy was cleansed. And Jesus said to him, “See that you say nothing to anyone, but go, show yourself to the priest and offer the gift that Moses commanded, for a proof to them.”
 
 When he had entered Capernaum, a centurion came forward to him, appealing to him, “Lord, my servant is lying paralyzed at home, suffering terribly.” And he said to him, “I will come and heal him.” But the centurion replied, “Lord, I am not worthy to have you come under my roof, but only say the word, and my servant will be healed. For I too am a man under authority, with soldiers under me. And I say to one, ‘Go,’ and he goes, and to another, ‘Come,’ and he comes, and to my servant, ‘Do this,’ and he does it.” When Jesus heard this, he marveled and said to those who followed him, “Truly, I tell you, with no one in Israel have I found such faith. I tell you, many will come from east and west and recline at table with Abraham, Isaac, and Jacob in the kingdom of heaven, while the sons of the kingdom will be thrown into the outer darkness. In that place there will be weeping and gnashing of teeth.” And to the centurion Jesus said, “Go; let it be done for you as you have believed.” And the servant was healed at that very moment.
@@ -59,6 +101,14 @@ And when he got into the boat, his disciples followed him. And behold, there aro
 
 And when he came to the other side, to the country of the Gadarenes, two demon-possessed men met him, coming out of the tombs, so fierce that no one could pass that way. And behold, they cried out, “What have you to do with us, O Son of God? Have you come here to torment us before the time?” Now a herd of many pigs was feeding at some distance from them. And the demons begged him, saying, “If you cast us out, send us away into the herd of pigs.” And he said to them, “Go.” So they came out and went into the pigs, and behold, the whole herd rushed down the steep bank into the sea and drowned in the waters. The herdsmen fled, and going into the city they told everything, especially what had happened to the demon-possessed men. And behold, all the city came out to meet Jesus, and when they saw him, they begged him to leave their region.
 `;
+
+export const Ephesians3 = `For this reason I, Paul, a prisoner of Christ Jesus on behalf of you Gentiles—assuming that you have heard of the stewardship of God’s grace that was given to me for you, how the mystery was made known to me by revelation, as I have written briefly. When you read this, you can perceive my insight into the mystery of Christ, which was not made known to the sons of men in other generations as it has now been revealed to his holy apostles and prophets by the Spirit. This mystery is that the Gentiles are fellow heirs, members of the same body, and partakers of the promise in Christ Jesus through the gospel.
+
+Of this gospel I was made a minister according to the gift of God’s grace, which was given me by the working of his power. To me, though I am the very least of all the saints, this grace was given, to preach to the Gentiles the unsearchable riches of Christ, and to bring to light for everyone what is the plan of the mystery hidden for ages in God, who created all things, so that through the church the manifold wisdom of God might now be made known to the rulers and authorities in the heavenly places. This was according to the eternal purpose that he has realized in Christ Jesus our Lord, in whom we have boldness and access with confidence through our faith in him. So I ask you not to lose heart over what I am suffering for you, which is your glory.
+
+For this reason I bow my knees before the Father, from whom every family in heaven and on earth is named, that according to the riches of his glory he may grant you to be strengthened with power through his Spirit in your inner being, so that Christ may dwell in your hearts through faith—that you, being rooted and grounded in love, may have strength to comprehend with all the saints what is the breadth and length and height and depth, and to know the love of Christ that surpasses knowledge, that you may be filled with all the fullness of God.
+
+Now to him who is able to do far more abundantly than all that we ask or think, according to the power at work within us, to him be glory in the church and in Christ Jesus throughout all generations, forever and ever. Amen.`;
 
 export const Revelation7 = `    After this I saw four angels standing at the four corners of the earth, holding back the four winds of the earth, that no wind might blow on earth or sea or against any tree. Then I saw another angel ascending from the rising of the sun, with the seal of the living God, and he called with a loud voice to the four angels who had been given power to harm earth and sea, saying, “Do not harm the earth or the sea or the trees, until we have sealed the servants of our God on their foreheads.” And I heard the number of the sealed, 144,000, sealed from every tribe of the sons of Israel:
 
@@ -85,7 +135,10 @@ Then one of the elders addressed me, saying, “Who are these, clothed in white 
      and he who sits on the throne will shelter them with his presence.
 They shall hunger no more, neither thirst anymore;
      the sun shall not strike them,
-     nor any scorching heat.
+     nor any scorching heat.	const 
+	test('Should not a line with more characters than the given maximum', () => {
+		expect().toBe(true);
+	});
 For the Lamb in the midst of the throne will be their shepherd,
      and he will guide them to springs of living water,
 and God will wipe away every tear from their eyes.”`;
