f05f1f0470b247f0a5396a9e572a3610b134522d
Austin Theriot <austinmtheriot@gmail.com>
2020-10-22 12:24:27 -0500

    converted <a>s to <ScrollLink>s

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,56 +1,57 @@
 import React from 'react';
 import styles from './About.module.scss';
 
+import { ScrollLink } from './ScrollLink';
+
 export const About = () => {
 	return (
 		<article className={styles.About}>
 			<h1>Memorize Scripture</h1>
 			<nav>
-				<h2>Table of Contents</h2>
 				<ol>
 					<li>
-						<a href='#introduction'>Introduction</a>
+						<ScrollLink to='introduction'>Introduction</ScrollLink>
 					</li>
 					<li>
-						<a href='#instructions'>Instructions</a>
+						<ScrollLink to='instructions'>Instructions</ScrollLink>
 					</li>
 					<li>
-						<a href='#how-it-works'>How It Works</a>
+						<ScrollLink to='how-it-works'>How It Works</ScrollLink>
 					</li>
 					<li>
-						<a href='#review'>Review</a>
+						<ScrollLink to='how-to-review'>How To Review</ScrollLink>
 					</li>
 					<li>
-						<a href='#more-resources'>More Resources</a>
+						<ScrollLink to='more-resources'>More Resources</ScrollLink>
 					</li>
 					<li>
-						<a href='#contributing'>Contributing</a>
+						<ScrollLink to='contributing'>Contributing</ScrollLink>
 					</li>
 				</ol>
 			</nav>
 
 			<section>
-				<h2 id='#introduction'>Introduction</h2>
+				<h2 id='introduction'>Introduction</h2>
 				<p>Content...</p>
 			</section>
 			<section>
-				<h2 id='#instructions'>Instructions</h2>
+				<h2 id='instructions'>Instructions</h2>
 				<p>Content...</p>
 			</section>
 			<section>
-				<h2 id='#how-it-works'>How It Works</h2>
+				<h2 id='how-it-works'>How It Works</h2>
 				<p>Content...</p>
 			</section>
 			<section>
-				<h2 id='#review'>Review</h2>
+				<h2 id='how-to-review'>How To Review</h2>
 				<p>Content...</p>
 			</section>
 			<section>
-				<h2 id='#resources'>More Resources</h2>
+				<h2 id='more-resources'>More Resources</h2>
 				<p>Content...</p>
 			</section>
 			<section>
-				<h2 id='#contributing'>Contributing</h2>
+				<h2 id='contributing'>Contributing</h2>
 				<p>Content...</p>
 			</section>
 		</article>
