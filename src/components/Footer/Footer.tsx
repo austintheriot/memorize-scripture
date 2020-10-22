ce5fdae975ba0ae973d763d1297780669832d058
Austin Theriot <austinmtheriot@gmail.com>
2020-10-22 10:19:01 -0500

    added ChiRho logo to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import chiRho from '../../images/chirho-light.svg';
 
 export const Footer = () => {
 	return (
-		<footer>
+		<footer className={styles.Footer}>
 			<img src={chiRho} alt='Memorize Scripture Logo: Chi Rho' />
 		</footer>
 	);
