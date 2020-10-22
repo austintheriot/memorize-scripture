53270a72bc88791a289db4a850fa9999349104c7
Austin Theriot <austinmtheriot@gmail.com>
2020-10-22 10:28:49 -0500

    fixed li spacing, added logo

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,21 +3,29 @@ import styles from './Menu.module.scss';
 
 import { Link } from 'react-router-dom';
 
+import chiRho from '../../images/chirho-light.svg';
+
 export const Menu = (props: any) => {
 	return (
 		<header
 			className={[styles.header, props.menuOpen ? styles.menuOpen : ''].join(
 				' '
 			)}>
+			<img src={chiRho} alt='Memorize Scripture Logo: Chi Rho' />
 			<ul className={styles.ul}>
 				<li className={styles.li}>
 					<Link to='/' className={styles.link}>
 						Home
 					</Link>
 				</li>
-				<li>
+				<li className={styles.li}>
 					<Link to='/about' className={styles.link}>
 						About
+					</Link>
+				</li>
+				<li className={styles.li}>
+					<Link to='/contact' className={styles.link}>
+						Contact
 					</Link>
 				</li>
 			</ul>
