0c26246907a3a05574f7bcc0e06170d22bf221fe
Austin Theriot <austinmtheriot@gmail.com>
2020-10-22 11:52:55 -0500

    replaced article with section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import styles from './About.module.scss';
 
 export const About = () => {
 	return (
-		<main className={styles.About}>
+		<article className={styles.About}>
 			<h1>Memorize Scripture</h1>
 			<nav>
 				<h2>Table of Contents</h2>
@@ -21,37 +21,38 @@ export const About = () => {
 						<a href='#review'>Review</a>
 					</li>
 					<li>
-						<a href='#resources'>More Resources</a>
+						<a href='#more-resources'>More Resources</a>
 					</li>
 					<li>
 						<a href='#contributing'>Contributing</a>
 					</li>
 				</ol>
 			</nav>
-			<article>
+
+			<section>
 				<h2 id='#introduction'>Introduction</h2>
 				<p>Content...</p>
-			</article>
-			<article>
+			</section>
+			<section>
 				<h2 id='#instructions'>Instructions</h2>
 				<p>Content...</p>
-			</article>
-			<article>
+			</section>
+			<section>
 				<h2 id='#how-it-works'>How It Works</h2>
 				<p>Content...</p>
-			</article>
-			<article>
+			</section>
+			<section>
 				<h2 id='#review'>Review</h2>
 				<p>Content...</p>
-			</article>
-			<article>
+			</section>
+			<section>
 				<h2 id='#resources'>More Resources</h2>
 				<p>Content...</p>
-			</article>
-			<article>
+			</section>
+			<section>
 				<h2 id='#contributing'>Contributing</h2>
 				<p>Content...</p>
-			</article>
-		</main>
+			</section>
+		</article>
 	);
 };
