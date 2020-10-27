6a8752704371e3bf562ab1488125ded29499dd73
Austin Theriot <austinmtheriot@gmail.com>
2020-10-27 13:16:53 -0500

    refactoring, cleaning Home and app structure

---
+++ src/utilities/localStorage.ts
@@ -0,0 +1,58 @@
+export const storePlaySpeed = (speed: number) => {
+	console.log(`Storing ${speed} as default playback speed`);
+	window.localStorage.setItem('speed', speed.toString());
+};
+
+export const getPlaySpeed = () => {
+	console.log('Retrieving playback speed from local storage');
+	const speed = window.localStorage.getItem('speed') || '1';
+	console.log(`Playback speed from local storage is ${speed}`);
+	return parseFloat(speed);
+};
+
+////////////////////////////
+
+interface TextObject {
+	title: string;
+	body: string;
+}
+
+type TextArray = TextObject[];
+
+export const storeMostRecentPassage = (title: string) => {
+	console.log(
+		`Storing ${title} as most the most recently accessed chapter in local storage`
+	);
+	window.localStorage.setItem('recent', title);
+};
+
+export const getTextArray = (title: string) => {
+	const textsString = window.localStorage.getItem('texts');
+	return textsString ? (JSON.parse(textsString) as TextArray) : [];
+};
+
+export const addToTextArray = (title: string, body: string) => {
+	console.log(`Checking if ${title} already exists in local storage`);
+	let passageIsInLocalStorage = !!getTextBody(title);
+	if (passageIsInLocalStorage) {
+		console.log(`${title} exists in local storage`);
+		return;
+	} else {
+		console.log(`${title} does not exist in local storage`);
+		console.log(`Adding ${title} to local storage`);
+		let textArray = getTextArray(title);
+		//store no more than 5 passages at a time
+		if (textArray.length === 5) textArray.pop();
+		textArray.unshift({
+			title,
+			body,
+		});
+		window.localStorage.setItem('texts', JSON.stringify(textArray));
+	}
+};
+
+export const getTextBody = (title: string) => {
+	const textArray = getTextArray(title);
+	const text = textArray.find((el) => el.title === title);
+	return text?.body;
+};
