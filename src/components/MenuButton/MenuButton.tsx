32cc9654b0085ae88c2f936ebd4667314a0d59ab
Austin Theriot <austinmtheriot@gmail.com>
2020-10-27 12:19:11 -0500

    moved menu state into Redux

--- src/components/MenuButton/MenuButton.tsx
+++ src/components/MenuButton/MenuButton.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import styles from './MenuButton.module.scss';
 
-export const MenuButton = (props: any) => {
+//Menu State
+import { useSelector, useDispatch } from 'react-redux';
+import { selectMenu, setMenuIsOpen } from '../../state/menuSlice';
+
+export const MenuButton = () => {
+	const dispatch = useDispatch();
+	const menu = useSelector(selectMenu);
+
+	const toggleMenuOpen = () => {
+		dispatch(setMenuIsOpen(!menu.isOpen));
+	};
+
 	return (
 		<button
-			className={[styles.button, props.menuOpen ? styles.menuOpen : ''].join(
-				' '
-			)}
-			onClick={props.handleClick}>
+			className={[styles.button, menu.isOpen ? styles.menuOpen : ''].join(' ')}
+			onClick={toggleMenuOpen}>
 			<span
-				className={[styles.span1, props.menuOpen ? styles.span1Open : ''].join(
+				className={[styles.span1, menu.isOpen ? styles.span1Open : ''].join(
 					' '
 				)}></span>
 			<span
-				className={[styles.span2, props.menuOpen ? styles.span2Open : ''].join(
+				className={[styles.span2, menu.isOpen ? styles.span2Open : ''].join(
 					' '
 				)}></span>
 			<span
-				className={[styles.span3, props.menuOpen ? styles.span3Open : ''].join(
+				className={[styles.span3, menu.isOpen ? styles.span3Open : ''].join(
 					' '
 				)}></span>
 		</button>
