d29cda8e7d717f7e517e39f4f31c56d227f3f279
Austin Theriot <austinmtheriot@gmail.com>
2020-11-02 11:03:08 -0600

    deleted useless imports

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './Message.module.scss';
 
 import { CloseButton } from '../CloseButton/CloseButton';
