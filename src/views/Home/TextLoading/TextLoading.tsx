9b45e84146dd792d6dc8602c7af5bf88e88cd7f3
Austin Theriot <austinmtheriot@gmail.com>
2020-11-03 16:45:44 -0600

    refactored components, got rid of storing clickedLine in local storage

---
+++ src/views/Home/TextLoading/TextLoading.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './TextLoading.module.scss';
+
+export const TextLoading = () => {
+	return (
+		<div className={styles.loadingMockContainer}>
+			<p></p>
+			<p></p>
+			<p></p>
+			<p></p>
+			<p></p>
+			<p></p>
+			<p></p>
+			<p></p>
+			<p></p>
+			<p></p>
+		</div>
+	);
+};
