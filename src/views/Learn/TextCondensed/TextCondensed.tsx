3facf997a82d4aac75ffb0fa7b009f39df4a2d35
Austin Theriot <austinmtheriot@gmail.com>
2020-11-10 07:26:04 -0600

    add analytics for clicked condensed line

--- src/views/Learn/TextCondensed/TextCondensed.tsx
+++ src/views/Learn/TextCondensed/TextCondensed.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FirebaseContext } from '../../../app/firebaseContext';
 
 //App State
 import { useSelector, useDispatch } from 'react-redux';
@@ -8,6 +9,7 @@ import { selectText, splitTextClicked } from '../../../app/textSlice';
 import styles from './TextCondensed.module.scss';
 
 export const TextCondensed = () => {
+	const { analytics } = useContext(FirebaseContext);
 	const dispatch = useDispatch();
 	const text = useSelector(selectText);
 
@@ -15,6 +17,9 @@ export const TextCondensed = () => {
 		e: React.MouseEvent<HTMLParagraphElement, MouseEvent>,
 		i: number
 	) => {
+		analytics.logEvent('condensed_line_clicked', {
+			clickedLine: text.clickedLine === i ? -1 : text.clickedLine,
+		});
 		if (text.clickedLine === i) return dispatch(splitTextClicked(-1));
 		dispatch(splitTextClicked(i));
 	};
