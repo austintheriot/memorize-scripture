75cdb61a15a4a12654275a1c4506b8f4fc2008b1
Austin Theriot <austinmtheriot@gmail.com>
2020-11-10 17:24:16 -0600

    created setup and cleanup for tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,28 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import { unmountComponentAtNode } from 'react-dom';
+
+let container: any = null;
+let assignMock: any = null;
+
+beforeEach(() => {
+	//Prevent error on window.scrollTo() not supported
+	assignMock = jest.fn();
+	delete window.scrollTo;
+	window.scrollTo = assignMock;
+
+	// setup a DOM element as a render target
+	container = document.createElement('div');
+	document.body.appendChild(container);
+});
+
+afterEach(() => {
+	// cleanup on exiting
+	unmountComponentAtNode(container);
+	container?.remove();
+	container = null;
+
+	//delete scrollTo mock function
+	assignMock.mockClear();
+});
