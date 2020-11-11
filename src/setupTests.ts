7a745edee987a2f6c744f361fcf1fd7e99d16ba3
Austin Theriot <austinmtheriot@gmail.com>
2020-11-11 13:10:52 -0600

    deleted unecessary cleanup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,9 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-import { unmountComponentAtNode } from 'react-dom';
+import { cleanup } from '@testing-library/react';
 
-let container: any = null;
+// import { unmountComponentAtNode } from 'react-dom';
+// let container: any = null;
+
 let assignMock: any = null;
 
 beforeEach(() => {
@@ -13,18 +15,9 @@ beforeEach(() => {
 	assignMock = jest.fn();
 	delete window.scrollTo;
 	window.scrollTo = assignMock;
-
-	// setup a DOM element as a render target
-	container = document.createElement('div');
-	document.body.appendChild(container);
 });
 
 afterEach(() => {
-	// cleanup on exiting
-	unmountComponentAtNode(container);
-	container?.remove();
-	container = null;
-
 	//delete scrollTo mock function
 	assignMock.mockClear();
 });
