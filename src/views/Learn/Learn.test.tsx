d66d296cd139a1b1f468e0c5061cc45fef92aa34
Austin Theriot <austinmtheriot@gmail.com>
2020-11-11 13:11:00 -0600

    added first test

--- src/views/Learn/Learn.test.tsx
+++ src/views/Learn/Learn.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import Learn from './Learn';
 
@@ -15,16 +16,27 @@ const firebaseContext = {
 	analytics,
 };
 
-test('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(
+const MockedLearn = () => {
+	return (
 		<FirebaseContext.Provider value={firebaseContext}>
 			<Provider store={store}>
 				<MemoryRouter>
 					<Learn />
 				</MemoryRouter>
 			</Provider>
-		</FirebaseContext.Provider>,
-		div
+		</FirebaseContext.Provider>
 	);
+};
+
+describe('Learn', () => {
+	test('Should render without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<MockedLearn />, div);
+	});
+
+	test('Should have title Learn', () => {
+		render(<MockedLearn />);
+		const title = screen.getByRole('heading', { name: /Learn/i });
+		expect(title).toHaveTextContent('Learn');
+	});
 });
