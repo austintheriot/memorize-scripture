f491d7ecc9d22c342530fdfe4b032b950c764b60
Austin Theriot <austinmtheriot@gmail.com>
2020-11-12 14:34:32 -0600

    moved audio controls to audioCommands.ts

--- src/app/audioCommands.ts
+++ src/app/audioCommands.ts
@@ -3,9 +3,15 @@ import {
 	spacebarPressed,
 	leftArrowPressed,
 	rightArrowPressed,
+	playButtonClicked,
+	pauseButtonClicked,
+	progressBarClicked,
+	speedButtonClicked,
 } from './audioSlice';
+import { storePlaySpeed } from './storage';
 
 export const togglePlayPause = (config: UtilityConfig) => {
+	if (config.textAudio.readyState !== 4) return;
 	if (config.textAudio.paused) {
 		config.textAudio.play();
 	} else {
@@ -14,19 +20,51 @@ export const togglePlayPause = (config: UtilityConfig) => {
 	config.dispatch(spacebarPressed());
 };
 
+export const play = (config: UtilityConfig) => {
+	if (config.textAudio.readyState !== 4) return;
+	config.textAudio.play();
+	config.dispatch(playButtonClicked());
+};
+
+export const pause = (config: UtilityConfig) => {
+	if (config.textAudio.readyState !== 4) return;
+	config.textAudio.pause();
+	config.dispatch(pauseButtonClicked());
+};
+
 export const rewind = (config: UtilityConfig) => {
+	if (config.textAudio.readyState !== 4) return;
 	const targetTime = Math.max(config.textAudio.currentTime - 5, 0);
 	config.dispatch(leftArrowPressed(targetTime / config.textAudio.duration));
 	config.textAudio.currentTime = targetTime;
 };
 
 export const forward = (config: UtilityConfig) => {
+	if (config.textAudio.readyState !== 4) return;
 	const targetTime = Math.min(
 		config.textAudio.currentTime + 5,
 		config.textAudio.duration - 0.01
 	);
 	config.dispatch(rightArrowPressed(targetTime / config.textAudio.duration));
 	config.textAudio.currentTime = targetTime;
+};
+
+export const beginning = (config: UtilityConfig) => {
+	if (config.textAudio.readyState !== 4) return;
+	config.textAudio.currentTime = 0;
+};
+
+export const position = (config: UtilityConfig, targetTime: number) => {
+	if (config.textAudio.readyState !== 4) return;
+	config.dispatch(progressBarClicked(targetTime));
+	config.textAudio.currentTime = config.textAudio.duration * targetTime;
+};
+
+export const speed = (config: UtilityConfig, targetSpeed: number) => {
+	if (config.textAudio.readyState !== 4) return;
+	config.textAudio.playbackRate = targetSpeed;
+	config.dispatch(speedButtonClicked(targetSpeed));
+	storePlaySpeed(targetSpeed);
 };
 
 export const handleKeyPress = (
--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -2,104 +2,94 @@ import React, { useContext } from 'react';
 import styles from './Controls.module.scss';
 
 //State
-import { FirebaseContext } from '../../app/firebaseContext';
-import { AudioContext } from '../../app/audioContext';
+import { FirebaseContext } from 'app/firebaseContext';
+import { AudioContext } from 'app/audioContext';
 import { useSelector, useDispatch } from 'react-redux';
+import { selectAudioSettings } from 'app/audioSlice';
 import {
-	selectAudioSettings,
-	rewindButtonClicked,
-	forwardButtonClicked,
-	progressBarClicked,
-	playButtonClicked,
-	pauseButtonClicked,
-	speedButtonClicked,
-} from '../../app/audioSlice';
-import { selectText, viewChangeButtonClicked } from '../../app/textSlice';
+	play,
+	pause,
+	rewind,
+	forward,
+	beginning,
+	position,
+	speed,
+} from 'app/audioCommands';
+import { selectText, viewChangeButtonClicked } from 'app/textSlice';
 
 //Custom icons
-import flipIcon from '../../icons/flip.svg';
-import beginningIcon from '../../icons/beginning.svg';
-import rewindIcon from '../../icons/rewind.svg';
-import pauseIcon from '../../icons/pause.svg';
-import playIcon from '../../icons/play.svg';
-import forwardIcon from '../../icons/forward.svg';
-import loadingIcon from '../../icons/loading.svg';
-import errorIcon from '../../icons/error.svg';
+import flipIcon from 'icons/flip.svg';
+import beginningIcon from 'icons/beginning.svg';
+import rewindIcon from 'icons/rewind.svg';
+import pauseIcon from 'icons/pause.svg';
+import playIcon from 'icons/play.svg';
+import forwardIcon from 'icons/forward.svg';
+import loadingIcon from 'icons/loading.svg';
+import errorIcon from 'icons/error.svg';
 
 //Utilities
-import { storePlaySpeed, storeShowCondensed } from '../../app/storage';
+import { storeShowCondensed } from '../../app/storage';
+import { UtilityConfig } from 'app/types';
 
 export const Controls = () => {
 	const dispatch = useDispatch();
 	const audioSettings = useSelector(selectAudioSettings);
 	const text = useSelector(selectText);
-	const { textAudio } = useContext(AudioContext);
+	const { textAudio, setTextAudio } = useContext(AudioContext);
 	const { analytics } = useContext(FirebaseContext);
 
+	const config: UtilityConfig = {
+		textAudio,
+		setTextAudio,
+		analytics,
+		dispatch,
+	};
+
 	const handlePlay = () => {
-		analytics.logEvent('play_button_pressed');
-		if (textAudio.readyState !== 4) return;
-		textAudio.play();
-		dispatch(playButtonClicked());
+		analytics.logEvent('play_button_clicked');
+		play(config);
 	};
 
 	const handlePause = () => {
-		analytics.logEvent('pause_buton_pressed');
-		if (textAudio.readyState !== 4) return;
-		textAudio.pause();
-		dispatch(pauseButtonClicked());
+		analytics.logEvent('pause_buton_clicked');
+		pause(config);
 	};
 
 	const handleRewind = () => {
-		analytics.logEvent('back_button_pressed');
-		if (textAudio.readyState !== 4) return;
-		const targetTime = Math.max(textAudio.currentTime - 5, 0);
-		dispatch(rewindButtonClicked(targetTime / textAudio.duration));
-		textAudio.currentTime = targetTime;
+		analytics.logEvent('back_button_clicked');
+		rewind(config);
 	};
 
 	const handleForward = () => {
-		analytics.logEvent('forward_button_pressed');
-		if (textAudio.readyState !== 4) return;
-		const targetTime = Math.min(
-			textAudio.currentTime + 5,
-			textAudio.duration - 0.01
-		);
-		dispatch(forwardButtonClicked(targetTime / textAudio.duration));
-		textAudio.currentTime = targetTime;
+		analytics.logEvent('forward_button_clicked');
+		forward(config);
 	};
 
 	const handleBeginning = () => {
-		analytics.logEvent('beginning_button_pressed');
-		if (textAudio.readyState !== 4) return;
-		textAudio.currentTime = 0;
+		analytics.logEvent('beginning_button_clicked');
+		beginning(config);
 	};
 
 	const handleAudioPositionChange = (
 		e: React.ChangeEvent<HTMLInputElement>
 	) => {
 		const targetTime: number = Number(e.currentTarget.value);
+		position(config, targetTime);
 		analytics.logEvent('progress_bar_clicked', {
 			targetTime,
 		});
-		if (textAudio.readyState !== 4) return;
-		dispatch(progressBarClicked(targetTime));
-		textAudio.currentTime = textAudio.duration * targetTime;
 	};
 
 	const handleSpeedChange = () => {
-		if (textAudio.readyState !== 4) return;
 		const targetSpeed = Math.max((audioSettings.speed + 0.25) % 2.25, 0.5);
-		analytics.logEvent('speed_button_pressed', {
+		speed(config, targetSpeed);
+		analytics.logEvent('speed_button_clicked', {
 			targetSpeed,
 		});
-		textAudio.playbackRate = targetSpeed;
-		dispatch(speedButtonClicked(targetSpeed));
-		storePlaySpeed(targetSpeed);
 	};
 
 	const handleViewChange = () => {
-		analytics.logEvent('flip_view_button_pressed', {
+		analytics.logEvent('flip_view_button_clicked', {
 			showCondensed: text.showCondensed,
 		});
 		const targetShowCondensed = !text.showCondensed;
