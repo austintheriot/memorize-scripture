2105870113422c4222fb8a4ebbd98cccfa7247ab
Austin Theriot <austinmtheriot@gmail.com>
2020-11-16 13:10:50 -0600

    configured to allow both jest andcypress

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,17 @@
 describe('My First Test', () => {
-	it('Visits the Kitchen Sink', () => {
+	it('clicking "type" shows the right headings', () => {
 		cy.visit('https://example.cypress.io');
+
+		cy.pause();
+
+		cy.contains('type').click();
+
+		// Should be on a new URL which includes '/commands/actions'
+		cy.url().should('include', '/commands/actions');
+
+		// Get an input, type into it and verify that the value has been updated
+		cy.get('.action-email')
+			.type('fake@email.com')
+			.should('have.value', 'fake@email.com');
 	});
 });
