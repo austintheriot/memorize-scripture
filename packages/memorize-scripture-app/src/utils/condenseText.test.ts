ac9afa5e2d71aac15f2a03481fe45462fc4ac7bf
Austin Theriot <austinmtheriot@gmail.com>
2024-05-29 06:45:47 -0500

    debugging vitest wasm import - wip

--- packages/memorize-scripture-app/src/utils/condenseText.test.ts
+++ packages/memorize-scripture-app/src/utils/condenseText.test.ts
@@ -1,11 +1,15 @@
 import { it, describe, expect } from "vitest";
+import wasmBinary from "@/compiled/text_condense_bg.wasm?url";
 import init, { Condenser } from "@/compiled/text_condense";
 
-init().then(() => {
-  describe("numLineBreaks", () => {
-    it("it should have truthy exports", () => {
-      expect(new Condenser()).toBeTruthy();
-    });
-    it("1: it preserves all line breaks");
+describe("numLineBreaks", () => {
+  it("it should instantiate", async () => {
+    console.log(wasmBinary);
+    const fetchedResult = await fetch(wasmBinary);
+    console.log(fetchedResult);
+    // const inited = await init(fetchedResult)
+    expect(fetchedResult).toBeTruthy();
   });
+  it("1: it preserves all line breaks");
 });
+
